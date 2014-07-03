66e4a0be6134b9108f7093173b8968b0224516de
Tom Strassner <tstrassner8@yahoo.com>
2014-07-03 09:52:37 -0500

    documentation for new classes

--- Caelus/CLWeatherHour.h
+++ Caelus/CLWeatherHour.h
@@ -8,8 +8,16 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ *  A CLWeatherHour object should be initialized using the custom initializer. The NSDictionary it expects should be
+ *  a dictionary containing information about an individual hour of weather, parsed out of the weather underground API
+ *  response for hourly weather.
+ *
+ *  This object holds various fields describing weather conditions for an individual hour.
+ */
 @interface CLWeatherHour : NSObject
-- (id)initWithHourDict:(NSDictionary *)hourDict;
+// custom initializer
+- (id)initWithHourDict:(NSDictionary *)hourDict;                // hourDict has to be from weather underground API
 
 @property (nonatomic) NSInteger hour;                           // hour of the day in 24 hour time
 @property (strong, nonatomic) NSString *weekdayNameAbbrev;      // i.e. Sun, Mon, Tue, Wed, Thu, Fri, Sat
