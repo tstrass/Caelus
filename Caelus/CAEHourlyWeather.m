a09f776b10fd49bccc6de2c3acfdc5366cc0f8e2
tomstrassner <tomstrassner@gmail.com>
2014-07-08 09:27:20 -0500

    minor defensive code additions

--- Caelus/CAEHourlyWeather.m
+++ Caelus/CAEHourlyWeather.m
@@ -25,17 +25,19 @@
  *  @param dict needs to be the response from weather underground API hourly feature
  */
 - (void)parseJSONDict:(NSDictionary *)dict {
-	NSDictionary *hourlyForecastDict = [dict objectForKey:@"hourly_forecast"];
-
-	if (hourlyForecastDict) {
-		NSMutableArray *weatherHours = [[NSMutableArray alloc] init];
-		// send each weather hour dictionary to CLWeatherHour for parsing, then add it to the array
-		for (NSDictionary *hourDict in hourlyForecastDict) {
-			CAEWeatherHour *weatherHour = [[CAEWeatherHour alloc] initWithHourDict:hourDict];
-			[weatherHours addObject:weatherHour];
-		}
-		self.weatherHours = [[NSArray alloc] initWithArray:weatherHours];
-	}
+    if (dict) {
+        NSDictionary *hourlyForecastDict = [dict objectForKey:@"hourly_forecast"];
+        
+        if (hourlyForecastDict) {
+            NSMutableArray *weatherHours = [[NSMutableArray alloc] init];
+            // send each weather hour dictionary to CLWeatherHour for parsing, then add it to the array
+            for (NSDictionary *hourDict in hourlyForecastDict) {
+                CAEWeatherHour *weatherHour = [[CAEWeatherHour alloc] initWithHourDict:hourDict];
+                [weatherHours addObject:weatherHour];
+            }
+            self.weatherHours = [[NSArray alloc] initWithArray:weatherHours];
+        }
+    }
 }
 
 @end
